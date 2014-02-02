b4fc68af63efb1854ddea386fd374527e5860e0a
Song Han <songhan@ip-10-232-145-173.us-west-2.compute.internal>
2014-02-01 19:22:22 -0800

    changed style

--- pa2/IntSet.java
+++ pa2/IntSet.java
@@ -1,5 +1,7 @@
 public interface IntSet {
-    boolean contains(int key);
-    void add(int key);
-    void remove(int key);
+   boolean contains(int key);
+   
+   void add(int key);
+   
+   void remove(int key);
 }
