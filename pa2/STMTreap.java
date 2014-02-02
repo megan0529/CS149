2ae019142f29972128e4791d4c185a6edb83cb12
Song Han <songhan@stanford.edu>
2014-02-01 21:10:03 -0800

    all implemented but slow

--- pa2/STMTreap.java
+++ pa2/STMTreap.java
@@ -1,3 +1,7 @@
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.deuce.Atomic;
+
 public class STMTreap implements IntSet {
    static class Node {
       final int key;
@@ -15,55 +19,84 @@ public class STMTreap implements IntSet {
       }
    }
    
-   private long randState = 0;
-   private Node root;
+   private AtomicLong randState = new AtomicLong(0);
+   private Node       root;
    
-   public synchronized boolean contains(final int key) {
+   @Atomic
+   public boolean contains(final int key) {
       Node node = root;
+      int nodeKey;
       while (node != null) {
-         if (key == node.key) {
+         nodeKey = node.key;
+         if (key == nodeKey) {
             return true;
          }
-         node = key < node.key ? node.left : node.right;
+         node = key < nodeKey ? node.left : node.right;
       }
       return false;
    }
    
-   public synchronized void add(final int key) {
+   @Atomic
+   public void add(final int key) {
       root = addImpl(root, key);
    }
    
+   @Atomic
+   public void remove(final int key) {
+      root = removeImpl(root, key);
+   }
+   
+   @Atomic
    private Node addImpl(final Node node, final int key) {
       if (node == null) {
          return new Node(key, randPriority());
       }
-      else if (key == node.key) {
-         // no insert needed
-         return node;
-      }
-      else if (key < node.key) {
-         node.left = addImpl(node.left, key);
-         if (node.left.priority > node.priority) {
-            return rotateRight(node);
-         }
-         return node;
-      }
       else {
-         node.right = addImpl(node.right, key);
-         if (node.right.priority > node.priority) {
-            return rotateLeft(node);
+         int nodeKey = node.key;
+         if (key == nodeKey) {
+            // no insert needed
+            return node;
          }
-         return node;
+         else if (key < nodeKey) {
+            final Node leftNode = addImpl(node.left, key);
+            if (node.left != leftNode) {
+               node.left = leftNode;
+               if (leftNode.priority > node.priority) {
+                  return rotateRight(node);
+               }
+            }
+            return node;
+         }
+         else {
+            final Node rightNode = addImpl(node.right, key);
+            if (node.right != rightNode) {
+               node.right = rightNode;
+               if (rightNode.priority > node.priority) {
+                  return rotateLeft(node);
+               }
+            }
+            return node;
+         }
+         
       }
    }
    
    private int randPriority() {
       // The constants in this 64-bit linear congruential random number
       // generator are from http://nuclear.llnl.gov/CNP/rng/rngman/node4.html
-      randState = randState * 2862933555777941757L + 3037000493L;
-      return (int) (randState >> 30);
+      while (true) {
+         long old_rand = randState.get();
+         long new_rand = old_rand * 2862933555777941757L + 3037000493L;
+         if (randState.compareAndSet(old_rand, new_rand)) {
+            return (int) (new_rand >> 30);
+         }
+      }
+      
+      // randState = randState * 2862933555777941757L + 3037000493L;
+      // return (int) (randState >> 30);
    }
    
+   @Atomic
    private Node rotateRight(final Node node) {
       // node nL
       // / \ / \
@@ -76,6 +109,7 @@ public class STMTreap implements IntSet {
       return nL;
    }
    
+   @Atomic
    private Node rotateLeft(final Node node) {
       final Node nR = node.right;
       node.right = nR.left;
@@ -83,47 +117,59 @@ public class STMTreap implements IntSet {
       return nR;
    }
    
-   public synchronized void remove(final int key) {
-      root = removeImpl(root, key);
-   }
-   
+   @Atomic
    private Node removeImpl(final Node node, final int key) {
       if (node == null) {
          // not present, nothing to do
          return null;
       }
-      else if (key == node.key) {
-         if (node.left == null) {
-            // splice out this node
-            return node.right;
-         }
-         else if (node.right == null) {
-            return node.left;
-         }
-         else {
-            // Two children, this is the hardest case. We will pretend
-            // that node has -infinite priority, move it down, then retry
-            // the removal.
-            if (node.left.priority > node.right.priority) {
-               // node.left needs to end up on top
-               final Node top = rotateRight(node);
-               top.right = removeImpl(top.right, key);
-               return top;
+      else {
+         final Node oriLeftNode = node.left;
+         final Node oriRightNode = node.right;
+         final int nodeKey = node.key;
+         
+         if (key == nodeKey) {
+            if (oriLeftNode == null) {
+               return oriRightNode;
+            }
+            else if (oriRightNode == null) {
+               return oriLeftNode;
             }
             else {
-               final Node top = rotateLeft(node);
-               top.left = removeImpl(top.left, key);
-               return top;
+               // Two children, this is the hardest case. We will pretend
+               // that node has -infinite priority, move it down, then retry
+               // the removal.
+               if (oriLeftNode.priority > oriRightNode.priority) {
+                  // oriLeftNode needs to end up on top
+                  final Node top = rotateRight(node);
+                  final Node newTopRightNode = removeImpl(top.right, key);
+                  if (top.right != newTopRightNode)
+                     top.right = newTopRightNode;
+                  return top;
+               }
+               else {
+                  final Node top = rotateLeft(node);
+                  final Node newTopLeftNode = removeImpl(top.left, key);
+                  if (top.left != newTopLeftNode)
+                     top.left = newTopLeftNode;
+                  return top;
+               }
             }
          }
-      }
-      else if (key < node.key) {
-         node.left = removeImpl(node.left, key);
-         return node;
-      }
-      else {
-         node.right = removeImpl(node.right, key);
-         return node;
+         else if (key < nodeKey) {
+            final Node leftNode = removeImpl(oriLeftNode, key);
+            if (oriLeftNode != leftNode) {
+               node.left = leftNode;
+            }
+            return node;
+         }
+         else {
+            final Node rightNode = removeImpl(oriRightNode, key);
+            if (oriRightNode != rightNode) {
+               node.right = rightNode;
+            }
+            return node;
+         }
       }
    }
 }
